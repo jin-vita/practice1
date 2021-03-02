f6d5bf6bc22a567e544e75be6d34c7d60ecd82e6
jin-vita <vita.unsnetworks@gmail.com>
2021-03-02 14:59:21 +0900

    try 11

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 6001
 
-app.get('/', (req, res) => res.send('asdfsadf World!'))
 
 // 외부에서 서버에 접근하면 public 폴더에 있는 것만 쓸 수 있게 함
 app.use('/', express.static(path.join(__dirname, 'public')))
@@ -19,6 +18,47 @@ app.use(express.urlencoded({ extended: true }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+// 라우팅 함수
+const router = express.Router()
+
+// list로 get 요청이 오면 실행
+router.route('/list').get(async (req, res) => {
+    const params = req.query
+    // ejs 파일에 data를 보내고 html파일로 변환해 클라이언트에 응답
+    req.app.render('list', { data: result }, (err, html) => {
+        if(err) {
+            console.error(`view 렌더링 오류 - ${err}`)
+            return
+        }
+        res.end(html)
+    })
+})
+
+// 요청 타입 별로 라우팅
+// JSON 형식 응답
+router.route('/search').get((req, res) => {
+    // get방식 parameter를 얻는 방법
+    const params = req.query
+    res.writeHead(200, {
+        'Content-Type': 'application/json;charset=utf-8'
+    })
+    res.end(JSON.stringify({
+        code: 200,
+        type: 'get',
+        data: params
+    }))
+}).post((req, res) => {
+    // post 방식 parameter를 얻는 방법
+    const params = req.body
+    res.writeHead(200, {
+        'Content-Type': 'application/json;charset=utf-8'
+    })
+    res.end(JSON.stringify({
+        code: 200,
+        type: 'post',
+        data: params
+    }))
+})
 
 
 app.listen(port, () => {
